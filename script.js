f20a1d1a464538404c825270c7739d6beb5a3da9
VeeraMahendra7 <140475884+VeeraMahendra7@users.noreply.github.com>
2025-10-16 16:32:04 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
 //your JS code here. If required.
+let p = document.getElementById('timer');
+
+setInterval(()=>{
+	const now = new Date();
+	
+	const year = now.getFullYear();
+	const month = String(now.getMonth() + 1).padStart(2, "0");
+	const day = String(now.getDate()).padStart(2, "0");
+	let hours = now.getHours();
+	const ampm = hours >= 12 ? "PM" : "AM";
+	hours = hours%12 || 12;
+	hours = String(hours).padStart(2, "0");
+	const minutes = String(now.getMinutes()).padStart(2, "0");
+	const seconds = String(now.getSeconds()).padStart(2, "0");
+
+	p.textContent = `${month}/${day}/${year}, ${hours}:${minutes}:${seconds} ${ampm}`
+},1000);
